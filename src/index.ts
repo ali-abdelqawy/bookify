ca31f9abc865bb07082f573f4960c6e3fae24ba9
Ali Khaled <ali.abdelqawy@outlook.com>
2024-08-09 16:01:28 +0300

    add index file

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata";
+import { createExpressServer } from "routing-controllers";
+import { join } from "path";
+
+const app = createExpressServer({
+  routePrefix: "/api",
+  validation: {
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  },
+  defaultErrorHandler: false,
+  middlewares: [join(`${__dirname}/src/core/middlewares/global/*.middleware.{js,ts}`)],
+  controllers: [join(`${__dirname}/**/*.controller.{js,ts}`)],
+});
+
+app.listen(process.env.PORT, () => console.log("api is up and running!")).setTimeout(Number(process.env.SERVER_TIMEOUT_MS));
